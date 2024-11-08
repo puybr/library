98736ceda9ad58c53863ac4f178b80dc497163de
puybr <56723522+puybr@users.noreply.github.com>
2024-11-09 00:43:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -228,7 +228,7 @@ bookLibrary.addEventListener('click', (e) => {
     // Listen for read status change
     if (e.target.classList.contains('status')) {
         const statusTargetName = e.target.parentNode.parentNode.parentNode.parentNode.childNodes[1].innerText;
-        let myLibrary = JSON.parse(localStorage.getItem('myLibrary'));
+        myLibrary = JSON.parse(localStorage.getItem('myLibrary'));
         const statusIndex = getBook(myLibrary, statusTargetName);
         console.log(myLibrary[statusIndex].read);
         if (myLibrary[statusIndex].read === true) {
